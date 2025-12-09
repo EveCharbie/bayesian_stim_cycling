add61ad0805c47ade7be231b5c3db2eeb2cda044
Eve <eve.charbie@gmail.com>
2025-12-09 11:57:48 -0500

    code to check the range to use

---
+++ helpers/stimulation_familiarization.py
@@ -0,0 +1,45 @@
+from pysciencemode import Rehastim2 as St
+from pysciencemode import Modes, Device
+
+from pysciencemode import Channel as Ch
+
+
+#  Create a channel
+list_channels = [
+    Ch(
+        mode=Modes.SINGLE,
+        no_channel=1,
+        amplitude=10,  # Modify here the intensity
+        pulse_width=250,  # 100, 500
+        enable_low_frequency=True,
+        name="Biceps",
+        device_type=Device.Rehastim2,
+    )
+]
+
+# Create our object Stimulator
+stimulator = St(
+    port="COM3", # Enter the port on which the stimulator is connected
+    show_log=False,
+)
+
+# Initialise the channel
+stimulator.init_channel(
+    stimulation_interval=200, list_channels=list_channels, low_frequency_factor=2
+)
+
+"""
+Start the stimulation.
+It is possible to :
+- Give a time after which the stimulation will be stopped but not disconnected.
+- Update the parameters of the channel by giving a new list of channels. The channel given must have been 
+  initialised first.
+"""
+stimulator.start_stimulation()
+# stimulator.start_stimulation(stimulation_duration=10, upd_list_channels=nw_list_channel)
+
+# Stop the stimulation if still running
+stimulator.pause_stimulation()
+stimulator.end_stimulation()
+stimulator.disconnect()
+stimulator.close_port()
